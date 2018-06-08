411fff3dc56af32f1cce6261f6128677a75a2dab
Ali Nazari <ali.nazari@aut.ac.ir>
2018-06-08 20:39:13 +0430

    complete the bot class

--- src/main/java/CeitGifBot.java
+++ src/main/java/CeitGifBot.java
@@ -1,11 +1,34 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class CeitGifBot extends TelegramLongPollingBot
 {
     public void onUpdateReceived(Update update)
     {
+        SendMessage message = new SendMessage();
+        String command = update.getMessage().getText();
+        if (command.equals("/com1"))
+        {
+            System.out.println(update.getMessage().getFrom().getFirstName());
+            message.setText(update.getMessage().getFrom().getFirstName());
+        }
+        if (command.equals("/com2"))
+        {
+            System.out.println(update.getMessage().getFrom().getLastName());
+            message.setText(update.getMessage().getFrom().getLastName());
+        }
 
+        message.setChatId(update.getMessage().getChatId());
+        try
+        {
+            execute(message);
+        }
+        catch (TelegramApiException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public String getBotUsername()
