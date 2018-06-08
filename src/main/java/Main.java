895415235b73c0c8c724ed30fba6bd71ccac5c9a
Ali Nazari <ali.nazari@aut.ac.ir>
2018-06-08 20:40:09 +0430

    complete the Main class and initial a Bot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,19 @@
+import org.telegram.telegrambots.*;
+import org.telegram.telegrambots.exceptions.*;
+
 public class Main
 {
+    public static void main(String[] args)
+    {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try
+        {
+            telegramBotsApi.registerBot(new CeitGifBot());
+        }
+        catch (TelegramApiException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
