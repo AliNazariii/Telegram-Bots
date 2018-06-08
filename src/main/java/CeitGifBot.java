5fe7ccc34d3cb8af2c9a55354967920f24aa22ec
Ali Nazari <ali.nazari@aut.ac.ir>
2018-06-08 20:38:21 +0430

    configure the bot class...it should extends TelegramLongPollingBot

--- src/main/java/CeitGifBot.java
+++ src/main/java/CeitGifBot.java
@@ -1,3 +1,20 @@
-public class CeitGifBot
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class CeitGifBot extends TelegramLongPollingBot
 {
+    public void onUpdateReceived(Update update)
+    {
+
+    }
+
+    public String getBotUsername()
+    {
+        return "ceit_gif_bot";
+    }
+
+    public String getBotToken()
+    {
+        return "618947597:AAGF6CoqWzGzj9rU__eTdl6zlWKDV-FUokU";
+    }
 }
