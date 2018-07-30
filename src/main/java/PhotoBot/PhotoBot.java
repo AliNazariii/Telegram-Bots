df41838033b5c09b386b2be7efe272b0269690b6
Ali Nazari <ali.nazari@aut.ac.ir>
2018-07-30 16:22:26 +0430

    my photos added instead of buttons

--- src/main/java/PhotoBot/PhotoBot.java
+++ src/main/java/PhotoBot/PhotoBot.java
@@ -22,13 +22,14 @@ public class PhotoBot extends TelegramLongPollingBot
         if (update.hasMessage() && update.getMessage().hasText())
         {
             // Set variables
-            String message_text = update.getMessage().getText();
-            long chat_id = update.getMessage().getChatId();
-            if (message_text.equals("/start"))
+            String textMessage = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            if (textMessage.equals("/start"))
             {
                 SendMessage message = new SendMessage() // Create a message object object
-                        .setChatId(chat_id)
-                        .setText(message_text);
+                        .setChatId(chatId)
+                        .setText("Bot Started!");
                 try
                 {
                     sendMessage(message); // Sending our message object to user
@@ -38,12 +39,75 @@ public class PhotoBot extends TelegramLongPollingBot
                     e.printStackTrace();
                 }
             }
-            else if (message_text.equals("/pic"))
+
+            else if (textMessage.equals("/markup"))
+            {
+                SendMessage message = new SendMessage() // Create a message object object
+                        .setChatId(chatId)
+                        .setText("Here is your keyboard");
+
+                // Create ReplyKeyboardMarkup object
+                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+                // Create the keyboard (list of keyboard rows)
+                List<KeyboardRow> keyboard = new ArrayList<>();
+
+                // Create a keyboard row
+                KeyboardRow row = new KeyboardRow();
+
+                // Set each button, you can also use KeyboardButton objects if you need something else than text
+                row.add("Ali and AmirHosein");
+                row.add("Group");
+                row.add("/hide");
+
+                // Add the first row to the keyboard
+                keyboard.add(row);
+
+                // Create another keyboard row
+                row = new KeyboardRow();
+
+                // Set each button for the second line
+                row.add("AmirHosein 1");
+                row.add("AmirHosein 2");
+                row.add("AmirHosein 3");
+
+                // Add the second row to the keyboard
+                keyboard.add(row);
+
+                // Create another keyboard row
+                row = new KeyboardRow();
+
+                // Set each button for the second line
+                row.add("Mo'een 1");
+                row.add("Mo'een 2");
+                row.add("Mo'een 3");
+
+                // Add the third row to the keyboard
+                keyboard.add(row);
+
+                // Set the keyboard to the markup
+                keyboardMarkup.setKeyboard(keyboard);
+
+                // Add it to the message
+                message.setReplyMarkup(keyboardMarkup);
+
+                try
+                {
+                    sendMessage(message); // Sending our message object to user
+                }
+                catch (TelegramApiException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+            else if (textMessage.equals("Ali and AmirHosein"))
             {
                 SendPhoto msg = new SendPhoto()
-                        .setChatId(chat_id)
-                        .setPhoto("AgADAgAD6qcxGwnPsUgOp7-MvnQ8GecvSw0ABGvTl7ObQNPNX7UEAAEC")
-                        .setCaption("Photo");
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADla4xG6G4-VKEwzg92gcm5vAioBoABHdGNu5pQ6GlTWABAAEC")
+                        .setCaption("Ali and AmirHosein");
+
                 try
                 {
                     sendPhoto(msg); // Call method to send the photo
@@ -53,49 +117,123 @@ public class PhotoBot extends TelegramLongPollingBot
                     e.printStackTrace();
                 }
             }
-            else if (message_text.equals("/markup"))
+            else if (textMessage.equals("Group"))
             {
-                SendMessage message = new SendMessage() // Create a message object object
-                        .setChatId(chat_id)
-                        .setText("Here is your keyboard");
-                // Create ReplyKeyboardMarkup object
-                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-                // Create the keyboard (list of keyboard rows)
-                List<KeyboardRow> keyboard = new ArrayList<>();
-                // Create a keyboard row
-                KeyboardRow row = new KeyboardRow();
-                // Set each button, you can also use KeyboardButton objects if you need something else than text
-                row.add("Row 1 Button 1");
-                row.add("Row 1 Button 2");
-                row.add("Row 1 Button 3");
-                // Add the first row to the keyboard
-                keyboard.add(row);
-                // Create another keyboard row
-                row = new KeyboardRow();
-                // Set each button for the second line
-                row.add("Row 2 Button 1");
-                row.add("Row 2 Button 2");
-                row.add("Row 2 Button 3");
-                // Add the second row to the keyboard
-                keyboard.add(row);
-                // Set the keyboard to the markup
-                keyboardMarkup.setKeyboard(keyboard);
-                // Add it to the message
-                message.setReplyMarkup(keyboardMarkup);
+                SendPhoto msg = new SendPhoto()
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADmq4xG6G4-VKrq45nm-9IxQ4soBoABI-P2NoPbgF4jF8BAAEC")
+                        .setCaption("Group");
+
                 try
                 {
-                    sendMessage(message); // Sending our message object to user
+                    sendPhoto(msg); // Call method to send the photo
                 }
                 catch (TelegramApiException e)
                 {
                     e.printStackTrace();
                 }
             }
-            else if (message_text.equals("Row 1 Button 1"))
+            else if (textMessage.equals("AmirHosein 1"))
             {
                 SendPhoto msg = new SendPhoto()
-                        .setChatId(chat_id)
-                        .setPhoto("AgADAgAD6qcxGwnPsUgOp7-MvnQ8GecvSw0ABGvTl7ObQNPNX7UEAAEC")
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADl64xG6G4-VJdKTMu04NdpJFUoBoABGkdqExeFvEuh20DAAEC")
+                        .setCaption("AmirHosein 1");
+
+                try
+                {
+                    sendPhoto(msg); // Call method to send the photo
+                }
+                catch (TelegramApiException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else if (textMessage.equals("AmirHosein 2"))
+            {
+                SendPhoto msg = new SendPhoto()
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADma4xG6G4-VJ0tPD_qWoNu72DnRoABNKvecu2XsV_k20DAAEC")
+                        .setCaption("AmirHosein 2");
+
+                try
+                {
+                    sendPhoto(msg); // Call method to send the photo
+                }
+                catch (TelegramApiException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else if (textMessage.equals("AmirHosein 3"))
+            {
+                SendPhoto msg = new SendPhoto()
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADna4xG6G4-VJvlL8qYWWWwKHmmxoABMK9qKBLyQkVgWoDAAEC")
+                        .setCaption("AmirHosein 3");
+
+                try
+                {
+                    sendPhoto(msg); // Call method to send the photo
+                }
+                catch (TelegramApiException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else if (textMessage.equals("Mo'een 1"))
+            {
+                SendPhoto msg = new SendPhoto()
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADmK4xG6G4-VLF9hsw0a53vlJINBoABITdAqLsq2t6HPQEAAEC")
+                        .setCaption("Mo'een 1");
+
+                try
+                {
+                    sendPhoto(msg); // Call method to send the photo
+                }
+                catch (TelegramApiException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else if (textMessage.equals("Mo'een 2"))
+            {
+                SendPhoto msg = new SendPhoto()
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADm64xG6G4-VLUDEvgXXomPwZKuhoABHeYd6eHFnxy71cBAAEC")
+                        .setCaption("Mo'een 2");
+
+                try
+                {
+                    sendPhoto(msg); // Call method to send the photo
+                }
+                catch (TelegramApiException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else if (textMessage.equals("Mo'een 3"))
+            {
+                SendPhoto msg = new SendPhoto()
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADnK4xG6G4-VKjc_KEIltTn5_NnhoABEMe12oUDO7sacQBAAEC")
+                        .setCaption("Mo'een 3");
+
+                try
+                {
+                    sendPhoto(msg); // Call method to send the photo
+                }
+                catch (TelegramApiException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            else if (textMessage.equals("Group"))
+            {
+                SendPhoto msg = new SendPhoto()
+                        .setChatId(chatId)
+                        .setPhoto("AgADBAADmq4xG6G4-VKrq45nm-9IxQ4soBoABI-P2NoPbgF4jF8BAAEC")
                         .setCaption("Photo");
 
                 try
@@ -107,13 +245,17 @@ public class PhotoBot extends TelegramLongPollingBot
                     e.printStackTrace();
                 }
             }
-            else if (message_text.equals("/hide"))
+
+            else if (textMessage.equals("/hide"))
             {
                 SendMessage msg = new SendMessage()
-                        .setChatId(chat_id)
+                        .setChatId(chatId)
                         .setText("Keyboard hidden");
+
                 ReplyKeyboardRemove keyboardMarkup = new ReplyKeyboardRemove();
+
                 msg.setReplyMarkup(keyboardMarkup);
+
                 try
                 {
                     sendMessage(msg); // Call method to send the photo
@@ -123,10 +265,11 @@ public class PhotoBot extends TelegramLongPollingBot
                     e.printStackTrace();
                 }
             }
+
             else
             {
                 SendMessage message = new SendMessage() // Create a message object object
-                        .setChatId(chat_id)
+                        .setChatId(chatId)
                         .setText("Unknown command");
                 try
                 {
@@ -138,29 +281,35 @@ public class PhotoBot extends TelegramLongPollingBot
                 }
             }
         }
+
+        // Message contains photo
         else if (update.hasMessage() && update.getMessage().hasPhoto())
         {
-            // Message contains photo
             // Set variables
-            long chat_id = update.getMessage().getChatId();
+            long chatId = update.getMessage().getChatId();
 
             List<PhotoSize> photos = update.getMessage().getPhoto();
-            String f_id = photos.stream()
+
+            String fileId = photos.stream()
                     .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
                     .findFirst()
                     .orElse(null).getFileId();
-            int f_width = photos.stream()
+
+            int fileWidth = photos.stream()
                     .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
                     .findFirst()
                     .orElse(null).getWidth();
-            int f_height = photos.stream()
+
+            int fileHeight = photos.stream()
                     .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
                     .findFirst()
                     .orElse(null).getHeight();
-            String caption = "file_id: " + f_id + "\nwidth: " + Integer.toString(f_width) + "\nheight: " + Integer.toString(f_height);
+
+            System.out.println("File_ID: " + fileId);
+            String caption = "Width: " + Integer.toString(fileWidth) + "\nHeight: " + Integer.toString(fileHeight);
             SendPhoto msg = new SendPhoto()
-                    .setChatId(chat_id)
-                    .setPhoto(f_id)
+                    .setChatId(chatId)
+                    .setPhoto(fileId)
                     .setCaption(caption);
             try
             {
@@ -176,12 +325,14 @@ public class PhotoBot extends TelegramLongPollingBot
     public String getBotUsername()
     {
         // Return bot username
-        return "ceit_gif_bot";
+        return "chiz_ha_bot";
+        //return "ceit_gif_bot";
     }
 
     public String getBotToken()
     {
         // Return bot token from BotFather
-        return "618947597:AAGF6CoqWzGzj9rU__eTdl6zlWKDV-FUokU";
+        return "681034837:AAHBAqu2lBXf0XMuxe-H55O9A2D_4C8QJSk";
+        //return "618947597:AAGF6CoqWzGzj9rU__eTdl6zlWKDV-FUokU";
     }
 }
